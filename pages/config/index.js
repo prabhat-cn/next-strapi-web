ae59add7eb2535325965ff03e18e14a247e23cf1
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 13:08:56 +0530

    🚑

--- pages/config/index.js
+++
@@ -1,5 +0,0 @@
-// export const API_URL = process.env.NEXTJS_PUBLIC_URL || 'http://localhost:1337';
-const API_URL =
-  process.env.NEXTJS_PUBLIC_URL || 'https://strapi-next-api.herokuapp.com';
-
-export default API_URL;
