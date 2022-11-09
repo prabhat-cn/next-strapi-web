5c8f65c492fda3fc0b0ecefbfc4894d051a7ab43
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 18:40:17 +0530

    ✨

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ function CustomDocument() {
         <meta charSet="UTF-8" />
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" /> */}
-        <title>VertiCard by TemplateMo</title>
+        {/* <title>VertiCard by TemplateMo</title> */}
 
         <link
           rel="stylesheet"
