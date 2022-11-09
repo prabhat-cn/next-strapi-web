39175a6adf4cd11f01dd95d89d328a6ee6b05542
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 13:44:18 +0530

    ✨ 🐛

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import '../styles/globals.css';
-import Header from './common/components/Header';
-import Footer from './common/components/Footer';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import Intro from './info';
+
 export default function Home() {
-  return <>Hi</>;
+  return (
+    <>
+      <Intro />
+    </>
+  );
 }
