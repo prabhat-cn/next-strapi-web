fc2111a55d92569d9d3f3b5edd09335931a90875
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 13:53:51 +0530

    🔒️ ⚡️

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>VertiCard by TemplateMo</title>
       </Head>
 
