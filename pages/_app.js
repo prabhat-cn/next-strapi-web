16e25986848ef91926badb11093f0083349a2349
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 13:21:42 +0530

    🚑

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import Head from 'next/head';
 import '../styles/globals.css';
+import Header from './common/components/Header';
+import Footer from './common/components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>VertiCard by TemplateMo</title>
+      </Head>
+
+      <div className="tm-page-container mx-auto">
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
