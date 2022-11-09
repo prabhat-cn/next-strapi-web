efea6b8093e60fa0ed6c62e0b82e6e3c5bd7d18d
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 12:59:56 +0530

    ♻️

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <title>VertiCard by TemplateMo</title>
       </Head>
 
--- pages/_document.js
+++ pages/_document.js
@@ -9,9 +9,9 @@ function CustomDocument() {
     <Html>
       <Head>
         <meta charset="UTF-8" />
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" /> */}
-        <title>VertiCard by TemplateMo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        {/* <title>VertiCard by TemplateMo</title> */}
 
         <link
           rel="stylesheet"
