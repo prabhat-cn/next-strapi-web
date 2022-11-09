f3569df6aca1c95a39754c02f7a6c5cfa8211fb8
prabhat-cn <prabhat@capitalnumbers.com>
2022-11-09 19:25:55 +0530

    spinner added

--- pages/galary.js
+++ pages/galary.js
@@ -1,36 +1,53 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from 'axios';
 import Link from 'next/link';
 import React, { Fragment, useEffect, useState } from 'react';
 import NavMenu from './components/NavMenu';
 import { API_URL } from '../config';
+import GrowSpinner from './components/Spinners';
 
 export default function Galary() {
   const [imgGalary, setImgGalary] = useState([]);
   const [gHeaderTitle, setGHeaderTitle] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const getGalary = () => {
+    setIsLoading(true);
     axios
       .get(`${API_URL}/api/vgalaries?populate=*`)
       .then((res) => {
         if (res?.status === 200) {
-          setImgGalary(res?.data);
+          if (res?.data?.data?.length === 0) {
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            setImgGalary(res?.data?.data);
+          }
         }
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   };
 
   const getGalaryHeader = () => {
+    setIsLoading(true);
     axios
       .get(`${API_URL}/api/vgalaryheaders`)
       .then((res) => {
         // console.log('gHead->', res);
         if (res?.status === 200) {
-          setGHeaderTitle(res?.data);
+          if (res?.data?.data?.length === 0) {
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            setGHeaderTitle(res?.data);
+          }
         }
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   };
 
@@ -41,133 +58,61 @@ export default function Galary() {
     return () => {};
   }, []);
 
+  console.log('gHeaderTitle->', gHeaderTitle);
+
   return (
     <div className="tm-section">
       <NavMenu />
       <div className="tm-content-container">
-        <div className="tm-content tm-content-2">
-          <p>{gHeaderTitle?.data[0]?.attributes?.gheadtitle}</p>
+        {isLoading ? (
+          <div style={{ textAlign: 'center', marginTop: 20, marginBottom: 20 }}>
+            <GrowSpinner />
+          </div>
+        ) : imgGalary ? (
+          <div className="tm-content tm-content-2">
+            {!gHeaderTitle ? (
+              <h3 style={{ textAlign: 'center' }}>No header detail found!</h3>
+            ) : (
+              <p>{gHeaderTitle?.data[0]?.attributes?.gheadtitle}</p>
+            )}
 
-          <div className="container-fluid">
-            <div className="row tm-gallery" id="tmGallery">
-              {imgGalary &&
-                imgGalary?.data?.map((iData, indx) => (
-                  <Fragment key={indx}>
-                    <div className="col-sm-6 tm-gallery-item">
-                      <figure className="effect-bubba">
-                        <img
-                          src={iData?.attributes?.image?.data?.attributes?.url}
-                          alt={iData?.attributes?.imageName}
-                          className="img-fluid"
-                        />
-                        <figcaption>
-                          <h2>{iData?.attributes?.imageName}</h2>
-                          <p>{iData?.attributes?.imageDetail}</p>
-                          <Link
-                            href={
+            <div className="container-fluid">
+              <div className="row tm-gallery" id="tmGallery">
+                {imgGalary &&
+                  imgGalary?.map((iData, indx) => (
+                    <Fragment key={indx}>
+                      <div className="col-sm-6 tm-gallery-item">
+                        <figure className="effect-bubba">
+                          <img
+                            src={
                               iData?.attributes?.image?.data?.attributes?.url
                             }
-                          >
-                            View more
-                          </Link>
-                        </figcaption>
-                      </figure>
-                    </div>
-                  </Fragment>
-                ))}
-              {/* <div className="col-sm-6 tm-gallery-item">
-                  <figure className="effect-bubba">
-                    <img
-                      src="/assets/img/gallery/gallery-img-02.jpg"
-                      alt="Gallery item"
-                      className="img-fluid"
-                    />
-                    <figcaption>
-                      <h2>
-                        Fresh <span>Bubba</span>
-                      </h2>
-                      <p>Bubba likes to appear out of thin air.</p>
-                      <Link href="/assets/img/gallery/gallery-img-02.jpg">
-                        View more
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="col-sm-6 tm-gallery-item">
-                  <figure className="effect-bubba">
-                    <img
-                      src="/assets/img/gallery/gallery-img-03.jpg"
-                      alt="Gallery item"
-                      className="img-fluid"
-                    />
-                    <figcaption>
-                      <h2>
-                        Fresh <span>Bubba</span>
-                      </h2>
-                      <p>Bubba likes to appear out of thin air.</p>
-                      <Link href="/assets/img/gallery/gallery-img-03.jpg">
-                        View more
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="col-sm-6 tm-gallery-item">
-                  <figure className="effect-bubba">
-                    <img
-                      src="/assets/img/gallery/gallery-img-04.jpg"
-                      alt="Gallery item"
-                      className="img-fluid"
-                    />
-                    <figcaption>
-                      <h2>
-                        Fresh <span>Bubba</span>
-                      </h2>
-                      <p>Bubba likes to appear out of thin air.</p>
-                      <Link href="/assets/img/gallery/gallery-img-04.jpg">
-                        View more
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="col-sm-6 tm-gallery-item">
-                  <figure className="effect-bubba">
-                    <img
-                      src="/assets/img/gallery/gallery-img-05.jpg"
-                      alt="Gallery item"
-                      className="img-fluid"
-                    />
-                    <figcaption>
-                      <h2>
-                        Fresh <span>Bubba</span>
-                      </h2>
-                      <p>Bubba likes to appear out of thin air.</p>
-                      <Link href="/assets/img/gallery/gallery-img-05.jpg">
-                        View more
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div>
-                <div className="col-sm-6 tm-gallery-item">
-                  <figure className="effect-bubba">
-                    <img
-                      src="/assets/img/gallery/gallery-img-06.jpg"
-                      alt="Gallery item"
-                      className="img-fluid"
-                    />
-                    <figcaption>
-                      <h2>
-                        Fresh <span>Bubba</span>
-                      </h2>
-                      <p>Bubba likes to appear out of thin air.</p>
-                      <Link href="/assets/img/gallery/gallery-img-06.jpg">
-                        View more
-                      </Link>
-                    </figcaption>
-                  </figure>
-                </div> */}
+                            alt={iData?.attributes?.imageName}
+                            className="img-fluid"
+                          />
+                          <figcaption>
+                            <h2>{iData?.attributes?.imageName}</h2>
+                            <p>{iData?.attributes?.imageDetail}</p>
+                            <Link
+                              href={
+                                iData?.attributes?.image?.data?.attributes?.url
+                              }
+                            >
+                              View more
+                            </Link>
+                          </figcaption>
+                        </figure>
+                      </div>
+                    </Fragment>
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="tm-content tm-content-2">
+            <h1 style={{ textAlign: 'center' }}>{'No data found!'}</h1>
+          </div>
+        )}
       </div>
     </div>
   );
--- pages/info.js
+++ pages/info.js
@@ -1,22 +1,33 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from 'axios';
 import Link from 'next/link';
 import React, { Fragment, useEffect, useState } from 'react';
 import NavMenu from './components/NavMenu';
 import { API_URL } from '../config';
+import GrowSpinner from './components/Spinners';
 
 export default function Intro() {
   const [homeData, setHomeData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
   const getHomeData = () => {
+    setIsLoading(true);
     axios
       .get(`${API_URL}/api/vhomes?populate=*`)
       .then((res) => {
         // console.log('home-D->', res);
         if (res?.status === 200) {
-          setHomeData(res?.data);
+          if (res?.data?.data?.length === 0) {
+            setIsLoading(false);
+          } else {
+            setIsLoading(false);
+            setHomeData(res?.data?.data);
+          }
         }
       })
       .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   };
 
@@ -30,14 +41,21 @@ export default function Intro() {
     <>
       <section className="tm-section">
         <NavMenu />
-        {homeData && homeData?.data ? (
+
+        {isLoading ? (
+          <div
+            className="tm-content-container"
+            style={{ textAlign: 'center', marginTop: 20, marginBottom: 20 }}
+          >
+            <GrowSpinner />
+          </div>
+        ) : homeData ? (
           <Fragment>
             <div className="tm-content-container">
               <figure className="mb-0">
                 <img
                   src={
-                    homeData?.data[0]?.attributes?.bannerImage?.data?.attributes
-                      ?.url
+                    homeData[0]?.attributes?.bannerImage?.data?.attributes?.url
                   }
                   alt="Image"
                   className="img-fluid tm-img"
@@ -45,17 +63,15 @@ export default function Intro() {
               </figure>
               <div className="tm-content">
                 <h2 className="tm-page-title">
-                  {homeData?.data[0]?.attributes?.headTitle}
+                  {homeData[0]?.attributes?.headTitle}
                 </h2>
-                <p className="mb-4">
-                  {homeData?.data[0]?.attributes?.homeDetail}
-                </p>
+                <p className="mb-4">{homeData[0]?.attributes?.homeDetail}</p>
               </div>
             </div>
           </Fragment>
         ) : (
           <div className="tm-content-container">
-            <h1 style={{ textAlign: 'center' }}>No data found!</h1>
+            <h1 style={{ textAlign: 'center' }}>{'No data found!'}</h1>
           </div>
         )}
       </section>
